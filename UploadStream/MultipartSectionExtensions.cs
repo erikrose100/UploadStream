72ecc9c7d80f2450854f884d66910b3595a51aa9
Erik Rose <40496972+erikrose100@users.noreply.github.com>
2024-04-28 02:05:14 -0400

    Dotnet8 (#1)
    * dotnet3.1 -> dotnet8 updates
    
    * add gha workflow
    
    * --no-build + -c debug for dotnet pack
    
    * no build for dotnet test
    
    * v3 -> v4 for action versions

--- UploadStream/MultipartSectionExtensions.cs
+++ UploadStream/MultipartSectionExtensions.cs
@@ -8,6 +8,7 @@ namespace UploadStream {
         public static Encoding GetEncoding(this MultipartSection section) {
             var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out MediaTypeHeaderValue mediaType);
             // UTF-7 is insecure and should not be honored. UTF-8 will succeed in most cases.
+            #pragma warning disable SYSLIB0001 // Returning  secure UTF-8, will leave it up to consuming clients to check encoding of uploaded media
             if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
                 return Encoding.UTF8;
 
